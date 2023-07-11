83757c5f6a7eb56102524f43088eed4d7df280c1
Liufat <dd5672003@gmail.com>
2023-07-11 11:44:46 +0800

    [新增]購物車功能/調整商品數量功能

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -24,6 +24,7 @@ const cartSlice = createSlice({
     decreaseItemQuantity(state, action) {
       // payload = pizzaId
       const item = state.cart.find((item) => item.pizzaId === action.payload);
+      if (item.quantity === 1) return;
       item.quantity--;
       item.totalPrice = item.quantity * item.unitPrice;
     },
@@ -50,3 +51,6 @@ export const getTotalCartPrice = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
 
 export const getCart = (state) => state.cart.cart;
+
+export const getCurrentQuantityById = (id) => (state) =>
+  state.cart.cart.find((item) => item.pizzaId === id);
--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,11 +1,15 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
-import { addItem } from "../cart/cartSlice";
+import { addItem, getCurrentQuantityById } from "../cart/cartSlice";
+import DeleteItem from "../cart/DeleteItem";
+import UpdateItemQuantity from "../cart/UpdateItemQuantity";
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
   const dispatch = useDispatch();
+
+  const currentQuantity = useSelector(getCurrentQuantityById(id));
 
   const handleAddCart = () => {
     const newItem = {
@@ -38,11 +42,21 @@ function MenuItem({ pizza }) {
               Sold out
             </p>
           )}
-          {!soldOut && (
-            <Button type="small" onClick={handleAddCart}>
-              Add to cart
-            </Button>
-          )}
+          {}
+          {!soldOut &&
+            (currentQuantity ? (
+              <div className="flex items-center gap-3 sm:gap-8">
+                <UpdateItemQuantity
+                  currentQuantity={currentQuantity.quantity}
+                  pizzaId={id}
+                />
+                <DeleteItem pizzaId={id} />
+              </div>
+            ) : (
+              <Button type="small" onClick={handleAddCart}>
+                Add to cart
+              </Button>
+            ))}
         </div>
       </div>
     </li>
